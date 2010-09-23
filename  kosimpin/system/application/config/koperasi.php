701675d8c32d97c9395fc5fa747f8070a4bc5bd3
satiri.a <satiri.a@6f51360b-5ed9-1ad9-0160-ef92550ebf37>
2010-09-23 03:11:33 +0000

    add configuration for application. all configuration should be in this file.

---
+++  kosimpin/system/application/config/koperasi.php
@@ -0,0 +1,15 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+| -------------------------------------------------------------------
+| Konfigurasi Koperasi
+| -------------------------------------------------------------------
+|
+*/
+
+$config['nama_koperasi'] = "Koperasi Telaga Mandiri";
+$config['alamat'] = "Telaga Mas - Harapan Baru, Bekasi 17123 ";
+$config['telpon'] = "021-99773814";
+$config['tahun_pendirian'] = "2003";
+
+/* End of file koperasi.php */
+/* Location: ./system/application/config/koperasi.php */

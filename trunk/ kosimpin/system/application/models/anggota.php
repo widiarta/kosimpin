360909462eb43168e12100a8628bdd2637490095
satiri.a <satiri.a@6f51360b-5ed9-1ad9-0160-ef92550ebf37>
2010-09-24 02:17:08 +0000

    introduce new Base_model. Basic Database model for save,get,update,delete

---
+++ trunk/ kosimpin/system/application/models/anggota.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Anggota a.k.a Member
+ */
+class Anggota extends Base_Model {
+
+    function __construct()
+    {
+        parent::__construct("anggota","id");
+    }
+
+    /**
+     * Create new code number for anggota
+     */
+    function new_number()
+    {
+        
+    }
+}
+?>

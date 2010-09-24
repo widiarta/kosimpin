36f6e3809dc080a4b210c51296caed9d89f75222
satiri.a <satiri.a@6f51360b-5ed9-1ad9-0160-ef92550ebf37>
2010-09-24 06:20:35 +0000

    add header.php and footer.php

---  kosimpin/system/application/views/default/menu.php
+++  kosimpin/system/application/views/default/menu.php
@@ -1,3 +1,3 @@
 <a href="<?php echo base_url();?>/index.php/ctabungan">Tabungan</a><br>
-<a href="<?php echo base_url();?>/index.php/cpinjaman">Pinjaman</a><br><br>
-<a href="<?php echo base_url();?>/index.php/main/logout">Logout</a><br>
+<a href="<?php echo base_url();?>/index.php/cpinjaman">Pinjaman</a><br>
+<a href="<?php echo base_url();?>/index.php/cpengeluaran">Pengeluaran</a><br><br>

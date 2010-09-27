7ab1de2fe50ab37e18aa2c70549708763ff1db0a
satiri.a <satiri.a@6f51360b-5ed9-1ad9-0160-ef92550ebf37>
2010-09-27 05:49:46 +0000

    update tabungan view. add new controller anggota.

--- trunk/ kosimpin/system/application/views/default/tabungan/home.php
+++ trunk/ kosimpin/system/application/views/default/tabungan/home.php
@@ -2,21 +2,21 @@
 include(APPPATH."views/default/header.php");
 ?>
 <body>
-<form>
+<form action="<?php echo base_url()?>/index.php/ctabungan/save">
 <table>
 <tr><td>Anggota</td>
 	<td>
-		<input type="text">
+		<input type="text" name="anggota">
 	</td>
 </tr>
 <tr><td>Tgl transaksi</td>
 	<td>
-		<input type="text">
+		<input type="text" name="tgltrans">
 	</td>
 </tr>
 <tr><td>jumlah</td>
 	<td>
-		<input type="text">
+		<input type="text" name="jumlah">
 	</td>
 </tr>
 <tr><td></td>
@@ -26,5 +26,42 @@ include(APPPATH."views/default/header.php");
 </tr>
 </table>
 </form>
+<b>Saldo Tabungan</b>
+<table width="300px">
+ <tr style="font-weight:bold;background-color:#EEEEEE;">
+     <td>No.</td>
+     <td>Anggota</td><td>Tabungan</td></tr>
+<?php
+  if($saldo_tabungan){
+  $total_saldo = 0;
+  $c=1;
+  foreach($saldo_tabungan as $saldo)
+  {
+
+    $total_saldo += $saldo->saldo;
+  ?>
+    <tr>
+        <td><?php echo $c; ?></td>
+        <td><a href='<?php echo base_url();?>/index.php/anggota/<?php echo $saldo->id_anggota; ?>'>
+            <?php echo $saldo->nama; ?>
+            </a>
+        </td>
+        <td>
+            <a href='<?php echo base_url();?>/index.php/ctabungan/detail/<?php echo $saldo->id_anggota; ?>'>
+            <?php echo number_format($saldo->saldo,"."); ?>
+            </a>
+        </td>
+        </tr>
+  <?php
+    $c++;
+  }
+  ?>
+    <tr style="font-weight:bold;background-color:#EEEEEE;">
+        <td></td>
+        <td>Total</td><td><?php echo number_format($total_saldo,"."); ?></td></tr>
+  <?php
+  }
+?>
+</table>
 </body>
 </html>

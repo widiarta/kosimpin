c615760b198c772e42682f00b434310641321738
satiri.a <satiri.a@6f51360b-5ed9-1ad9-0160-ef92550ebf37>
2010-10-05 03:43:06 +0000

    add PHPExcel library

---
+++  kosimpin/system/libraries/phpexcel_loader.php
@@ -0,0 +1,14 @@
+<?php
+include("PHPExcel.php");
+include("PHPExcel/IOFactory.php");
+
+/**
+ Dummy class
+*/
+class Phpexcel_Loader 
+{
+	function __construct(){
+		$objPHPExcel = new PHPExcel();
+	}
+}
+?>

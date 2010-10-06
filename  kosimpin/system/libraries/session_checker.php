8e4f3cf92eca9244df217979c21eada949d424f5
satiri.a <satiri.a@6f51360b-5ed9-1ad9-0160-ef92550ebf37>
2010-10-06 10:50:55 +0000

    add fooStack PHPunit test

---
+++  kosimpin/system/libraries/session_checker.php
@@ -0,0 +1,40 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Session checker
+ * Just redirect to login when no session created
+ *
+ * @package		libs
+ * @author      Iwan Rahardi,Ahmad Satiri
+ * @copyright	Copyright (c) 2010, StreetDirectory.com
+ * @link		http://www.streetdirectory.com
+ * @since		Version 1.0
+ * @filesource
+ */
+class Session_checker
+{
+
+	function __construct()
+	{		
+		$CI = &get_instance();
+		$this->RTR =& load_class('Router');
+		$this->output = $CI->output;
+		$this->session = $CI->session;
+		$this->config = $CI->config;
+		
+		$dir = 	$this->RTR->fetch_directory();
+		$class  = $this->RTR->fetch_class();
+		$method = $this->RTR->fetch_method();		
+		$path = $dir."$class/$method";
+		
+		$page_init = $this->config->item("page_init");
+		
+		if(!in_array($path,$page_init))
+		{		
+			$session_var = $this->config->item('session_var_to_watch');
+			if(!($this->session->userdata($session_var)))
+			{
+				redirect('main', NULL);
+			}
+		}
+	}
+}

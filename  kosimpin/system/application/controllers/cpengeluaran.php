78a40bad423a26e8dad8099ec04e2c346d50f55d
satiri.a <satiri.a@6f51360b-5ed9-1ad9-0160-ef92550ebf37>
2010-10-12 02:05:06 +0000

    implement pengeluaran

---
+++  kosimpin/system/application/controllers/cpengeluaran.php
@@ -0,0 +1,49 @@
+<?php
+class cpengeluaran extends common
+{
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model("pengeluaran");
+		$this->load->model("user");
+		$this->load->model("anggota");
+		
+		$this->pengeluaran->init_glaccount("001","006");
+	}
+	
+	function index()
+	{
+		$this->load->helper(array('form', 'url','tanggal'));
+		$this->load->library('form_validation');
+
+		
+		$data = array();
+
+		$this->form_validation->set_rules('tanggal', 'tanggal', 'required');
+		$this->form_validation->set_rules('bulan', 'Bulan', 'required');
+		$this->form_validation->set_rules('tahun', 'Tahun', 'required');
+		$this->form_validation->set_rules('jumlah', 'Jumlah', 'required');
+		
+		if ($this->form_validation->run() == FALSE)
+		{		
+			$data["data_pengeluaran"] = $this->pengeluaran->get_total_per_month();
+			$this->_load_view('pengeluaran',$data);	
+		}
+		else
+		{
+			//pengeluaran baru
+			//tanggal
+			$tanggal = $this->input->post("tahun")."-".$this->input->post("bulan")."-".$this->input->post("tanggal");
+			
+			//sukses
+			$data = array(
+				"jumlah" => $this->input->post("jumlah"),
+				"tgl_transaksi" => "$tanggal"
+				);
+				
+			$this->pengeluaran->save($data);
+			redirect("cpengeluaran/index");		
+		}
+	}
+
+}
---
+++  kosimpin/system/application/models/pengeluaran.php
@@ -0,0 +1,64 @@
+<?php
+class Pengeluaran extends Base_model 
+{
+	var $kode_account;
+	var $cash_account;
+	
+    function __construct()
+    {
+        parent::__construct();
+        $this->init("pengeluaran", "id");
+		$this->load->model("gl/Gledger");
+    }
+	
+	function init_glaccount($cash_account,$tabungan_account)
+	{
+		$this->kode_account = $tabungan_account;
+		$this->cash_account = $cash_account;
+	}
+	
+	/**
+	* Overload Parent
+	*/
+	function save($data)
+	{
+		parent::save($data);
+		if(is_array($data))
+		{
+			$data = (Object)$data;
+		}
+				
+		//save gl		
+		$kas = new Jurnal_entry();
+		$kas->nomor_account = $this->cash_account;
+		$kas->kredit_value = (float)$data->jumlah;
+		$kas->debit_value = 0;
+		$kas->tgl_transaksi = $data->tgl_transaksi;
+		$kas->nomor_dokumen = "";
+
+		$pengeluaran = new Jurnal_entry();
+		$pengeluaran->nomor_account = $this->kode_account;
+		$pengeluaran->debit_value = (float)$data->jumlah;
+		$pengeluaran->kredit_value = 0;
+		$pengeluaran->tgl_transaksi = $data->tgl_transaksi;
+		$pengeluaran->nomor_dokumen = "";
+		
+		//kas berkurang
+		//pengeluaran bertambah
+		$result = $this->Gledger->write_jurnal($pengeluaran,$kas);		
+	}
+	
+	function get_total_per_month()
+	{
+		$fields="sum(jumlah) as sum_jumlah";
+		$this->db->select($fields)->from($this->table_name)
+		                          ->group_by("month(tgl_transaksi)");
+		$rec = $this->db->get();
+		if($rec->num_rows()>0)
+		{
+			return $rec->result();
+		}
+		return FALSE;
+	}
+}
+?>

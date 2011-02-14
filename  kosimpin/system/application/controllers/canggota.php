96597e73fd23ddd7b06f950c8c925232e4006810
satiri.a <satiri.a@6f51360b-5ed9-1ad9-0160-ef92550ebf37>
2011-02-14 04:15:43 +0000

    update anggota. add some views.

---  kosimpin/system/application/controllers/canggota.php
+++  kosimpin/system/application/controllers/canggota.php
@@ -8,6 +8,7 @@ class canggota extends Common {
 		$this->load->model("anggota");
 		$this->load->model("pinjaman");
         $this->load->model("tabungan");
+		 $this->load->model("jenis_tabungan");
 	}
 
 	/**
@@ -31,5 +32,55 @@ class canggota extends Common {
 		$data["tabungan"] = $this->tabungan->get_saldo_per_anggota(null,$id_anggota);
 		$data["pinjaman"] = $this->pinjaman->get_saldo_per_anggota($id_anggota);
 		$this->_load_view('anggota/per_orang',$data);
+	}
+	
+	function tabdetail($jenis_tabungan=null)
+	{
+		if($jenis_tabungan!=null)
+		{
+			//1. sukarela,2. wajib, 3. pokok
+			$id_anggota = $this->session->userdata("id_anggota");
+			$detail = $this->tabungan->get_detail_per_anggota($id_anggota,$jenis_tabungan);
+			
+			if($jenis_tabungan!=null)
+			{
+				$result = $this->jenis_tabungan->get_by_id($jenis_tabungan);
+				$jenis = $result[0];
+			}
+			else
+			{
+				$jdata = array("jenis_tabungan"=>"Total Simpanan");
+				$jenis = (Object) $jdata;
+			}
+			
+			$data["detail_transaksi"] = $detail;
+			$data["jenis_tabungan"] = $jenis;
+			$data["id_jenis"] = $jenis; 
+			$data["nama_anggota"] = $this->anggota->get_name($id_anggota);
+			$this->_load_view('tabungan/rinci_per_anggota',$data);			
+		}
+	}
+	
+	function pinjdetail()
+	{
+		$id_anggota = $this->session->userdata("id_anggota");
+		$data = array();
+		$data["data_pinjaman"] = $this->pinjaman->get_saldo_per_pinjaman($id_anggota);	
+		$data["nama_anggota"] = $this->anggota->get_name($id_anggota); 
+		$this->_load_view('pinjaman/anggota_per_anggota',$data);	
+	}
+	
+	function detpinjaman($id=null)
+	{
+		if($id!=null)
+		{
+			$id_anggota = $this->session->userdata("id_anggota");
+			$data = array();
+			$data["pembayaran"] = $detail_bayar = $this->pinjaman->get_detail_pembayaran($id);
+			$data["pinjaman"] = $this->pinjaman->get_by_id($id);
+			$data["nama_anggota"] = $this->anggota->get_name($id_anggota); 
+			$this->_load_view('pinjaman/history_bayar',$data);	
+		
+		}
 	}
 }
---
+++  kosimpin/system/application/views/default/pinjaman/history_bayar.php
@@ -0,0 +1,53 @@
+<?php
+include(APPPATH."views/default/header.php");
+?>
+<body>
+    <b>Pembayaran Pinjaman : <?php echo $nama_anggota;  ?></b>
+    <table>
+	<tr><td>Untuk Pinjaman</td><td>:</td><td><b><?php echo date("d-M-y",strtotime($pinjaman[0]->tgl_transaksi));?></b></td></tr>
+	<tr><td>Jumlah Pinjaman</td><td>:</td><td align='right'><b><?php echo number_format($pinjaman[0]->jumlah_pinjaman,0); ?></b></td></tr>	
+	<tr><td>Jumlah Jasa</td><td>:</td><td align='right'><b><?php echo number_format($pinjaman[0]->jumlah_jasa,0); ?></b></td></tr>	
+	<tr><td>Saldo Hutang</td><td>:</td><td align='right'><b><?php echo number_format($pinjaman[0]->saldo,0); ?></b></td></tr>
+	</table>
+	<br/>
+	
+	History Pembayaran  
+	<!-- history pembayaran -->
+	<table width='300px' style='border:solid 1px #CCCCCC;'>
+	<tr style="font-weight:bold;background-color:#EEEEEE;">
+		<td align='center'>No.</td>
+		<td align='center'>Tgl.</td>
+		<td align='center'>J</td>
+		<td align='center'>Jml</td>
+	</tr>
+	<?php 
+		$total_bayar = 0;
+		if($pembayaran)
+		{
+			$c=1;
+			foreach($pembayaran as $byr)
+			{
+			$total_bayar += $byr->jumlah_pembayaran;
+	?>
+		<tr>
+		<td align='center'><?php echo $c?>.</td>
+		<td><?php echo date("d-M-y",strtotime($byr->tgl_transaksi)); ?></td>
+		<td align='center'><?php echo $byr->jenis_pembayaran; ?></td>
+		<td align='right'><?php echo number_format($byr->jumlah_pembayaran,","); ?></td>
+		</tr>
+		
+	<?php 
+			$c++;
+			}
+		}
+	?>
+	<tr style="font-weight:bold;background-color:#EEEEEE;">
+		<td align='center' colspan='3'>Total Pembayaran</td>
+		<td align='right'><?php echo number_format($total_bayar,","); ?></td>
+	</tr>	
+	</table>
+</body>
+<?php
+include(APPPATH."views/default/footer.php");
+?>
+</html>
